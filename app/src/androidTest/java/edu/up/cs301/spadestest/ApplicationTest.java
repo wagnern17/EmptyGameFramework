bd17de56a5f2a5903e4458d675acd403e06c5b3e
wagnern17 <wagnern17@up.edu>
2015-11-08 14:35:01 -0800

    Imported Game Framework

---
+++ app/src/androidTest/java/edu/up/cs301/spadestest/ApplicationTest.java
@@ -0,0 +1,13 @@
+package edu.up.cs301.spadestest;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
