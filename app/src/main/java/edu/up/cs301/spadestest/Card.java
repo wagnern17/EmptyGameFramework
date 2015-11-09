45631f496e500b68f6542df2c20b5b8f53df3b57
wagnern17 <wagnern17@up.edu>
2015-11-08 17:22:57 -0800

    Card class partially implemented

--- app/src/main/java/edu/up/cs301/spadestest/Card.java
+++ app/src/main/java/edu/up/cs301/spadestest/Card.java
@@ -5,7 +5,41 @@ package edu.up.cs301.spadestest;
  */
 public class Card {
 
-    public Card() {
+    private int rank;
+    private String suit;
 
+    /**
+     * ctor for Card object
+     * @param r int that holds the rank of a Card object, 2-13
+     * @param s string that holds the suit of a Card object
+     */
+    public Card(int r, String s) {
+        rank = r;
+        suit = s;
+    }
+
+    public int getRank(){
+        int temp = this.rank;
+        return temp;
+    }
+
+    public String getSuit(){
+        String temp = this.suit;
+        return temp;
+    }
+
+    public void setRank(int r){
+        this.rank = r;
+    }
+
+    public void setSuit(String s){
+        this.suit = s;
+    }
+
+    public void rankCheck(Card c){
+        if(c.getRank() < 2 || c.getRank() > 13){
+            c.setRank(2);
+        }
+        return;
     }
 }
